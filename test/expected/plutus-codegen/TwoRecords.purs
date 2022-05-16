a40f9805954770791329412470992905250f88ee
Drazen Popovic <bladyjoker@gmail.com>
2022-05-16 11:09:43 +0200

    Revert "forgot to add the formatted/linted files"
    This reverts commit b7659d429fec34168f3bbe0ac8b03fe636ba9e4c.

--- test/expected/plutus-codegen/TwoRecords.purs
+++ test/expected/plutus-codegen/TwoRecords.purs
@@ -9,61 +9,36 @@ import Data.Show.Generic (genericShow)
 import Data.Tuple (Tuple(Tuple))
 import FromData (class FromData, genericFromData)
 import ToData (class ToData, genericToData)
-import TypeLevel.DataSchema
-  ( ApPCons
-  , Field
-  , I
-  , Id
-  , IxK
-  , MkField
-  , MkField_
-  , MkIxK
-  , MkIxK_
-  , PCons
-  , PNil
-  , PSchema
-  , class HasPlutusSchema
-  , type (:+)
-  , type (:=)
-  , type (@@)
-  )
+import TypeLevel.DataSchema (ApPCons, Field, I, Id, IxK, MkField, MkField_, MkIxK, MkIxK_, PCons, PNil, PSchema, class HasPlutusSchema, type (:+), type (:=), type (@@))
 import TypeLevel.Nat (S, Z)
 
 data TwoRecords
   = FirstRecord
-      { _fra :: String
-      , _frb :: Int
-      }
+    { _fra :: String
+    , _frb :: Int
+    }
   | SecondRecord
-      { _src :: Int
-      , _srd :: Array Int
-      }
+    { _src :: Int
+    , _srd :: Array Int
+    }
 
 derive instance Generic TwoRecords _
 
 instance Show TwoRecords where
   show a = genericShow a
 
-instance
-  HasPlutusSchema TwoRecords
-    ( "FirstRecord"
-        :=
-          ( "_fra" := I String
-              :+ "_frb"
-              := I Int
-              :+ PNil
-          )
-        @@ (Z)
-        :+ "SecondRecord"
-        :=
-          ( "_src" := I Int
-              :+ "_srd"
-              := I (Array Int)
-              :+ PNil
-          )
-        @@ (S (Z))
-        :+ PNil
-    )
+instance HasPlutusSchema TwoRecords
+  ("FirstRecord" :=
+     ("_fra" := I String
+     :+ "_frb" := I Int
+     :+ PNil)
+   @@ (Z)
+  :+ "SecondRecord" :=
+       ("_src" := I Int
+       :+ "_srd" := I (Array Int)
+       :+ PNil)
+     @@ (S (Z))
+  :+ PNil)
 
 instance ToData TwoRecords where
   toData x = genericToData x
