ca2e2a629192aa2482ca6913f6843b8d0f7041c5
Drazen Popovic <bladyjoker@gmail.com>
2022-05-19 16:13:44 +0200

    Unflake the plutus-extra input due to a build err
    error: derivation '/nix/store/p61f2ji9kjkpmnarwj41nvyc7pdncawh-hoogle-with-packages.drv' requires non-existent output 'doc' from input derivation '/nix/store/02bvcjhr2hjyw1j7d1xigr1rjsbpz73r-flat-lib-flat-0.4.5-haddock.drv'

--- flake.nix
+++ flake.nix
@@ -24,7 +24,10 @@
       flake = false;
     };
 
-    plutus-extra.url = "github:Liqwid-Labs/plutus-extra";
+    plutus-extra = {
+      url = "github:Liqwid-Labs/plutus-extra";
+      flake = false;
+    };
   };
 
   outputs = inputs@{ self, flake-utils, haskell-nix, ... }:
